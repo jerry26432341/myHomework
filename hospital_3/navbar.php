7ab13e8d10fa9236bc29a5dca57868ee34d40084
pin chiao <jerry26432341@gmail.com>
2021-08-20 15:26:06 +0800

    更新

---
+++ hospital_3/navbar.php
@@ -0,0 +1,39 @@
+<?php
+require_once "index.php";
+
+// 如果沒有啟用 session, 就然它啟用
+if(! isset($_SESSION)) {
+    session_start();
+}
+
+?>
+
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">找醫院</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="add.html">新增資料</a>
+                </li>
+            </ul>
+        </div>
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="login.php">登入</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">註冊</a>
+            </li>
+        </ul>
+    </div>
+</nav>
